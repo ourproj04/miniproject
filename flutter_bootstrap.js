6ea2888f8e63e6e44b5e5cf8daeee1f132879b94
Joel Biju <joelbiju547@gmail.com>
2024-11-08 22:21:05 +0530

    quantity variable added

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2187760257"
+    serviceWorkerVersion: "936478881"
   }
 });
