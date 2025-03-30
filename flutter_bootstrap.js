9fc06096c128b68d8ef06e4644f4cf13f4db4917
Joel Biju <joelbiju547@gmail.com>
2025-03-30 21:25:50 +0530

    quick locate files added

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2769843121"
+    serviceWorkerVersion: "3601362448"
   }
 });
